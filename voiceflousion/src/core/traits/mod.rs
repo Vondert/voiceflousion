f33927579b1732400bce38a203a07e9146aa4f61
Vondert <25042018avic@gmail.com>
2024-08-04 19:45:38 +0300

    README.md and Cargo.toml changed for 0.2.0 version. CHANGELOG.md added

--- voiceflousion/src/core/traits/mod.rs
+++ voiceflousion/src/core/traits/mod.rs
@@ -5,10 +5,15 @@ mod responder;
 
 pub use self::update::Update;
 pub use self::client::Client;
-pub use self::sender::Sender;
 pub use self::responder::Responder;
 
 #[cfg(feature = "advanced")]
 pub use self::{
+    sender::Sender,
     client::get_last_sent_message
+};
+
+#[cfg(not(feature = "advanced"))]
+pub(crate) use self::{
+    sender::Sender
 };
--- voiceflousion/src/integrations/telegram/mod.rs
+++ voiceflousion/src/integrations/telegram/mod.rs
@@ -3,7 +3,16 @@ mod telegram_client;
 mod telegram_sender;
 mod telegram_responder;
 
+#[cfg(feature = "advanced")]
+pub use self::{
+    telegram_sender::TelegramSender
+};
+
+#[cfg(not(feature = "advanced"))]
+pub(super) use self::{
+    telegram_sender::TelegramSender
+};
+
 pub use self::telegram_client::TelegramClient;
 pub use self::telegram_update::TelegramUpdate;
-pub use self::telegram_sender::TelegramSender;
 pub use self::telegram_responder::TelegramResponder;
